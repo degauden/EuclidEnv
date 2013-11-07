2ef202be5afa149a37bdfd4bf36864858b8f73ee
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2013-11-07 10:52:33 +0100

    added the 2 parts. One for the shell only and one for the login

--- data/profile/euclid.csh
+++ data/profile/euclid.csh
@@ -4,6 +4,25 @@ if ( ! -e ${HOME}/.noEuclidLoginScript ) then
     setenv EUCLID_SYS_CONFIG=/etc/sysconfig/euclid
   endif
 
+
+  # shell part. has to deal with the shell settings. Pretty much everything but 
+  # the environment variables. The script can be manually called from .tcshrc
+  set shellscr=`/usr/bin/which Euclid_group_setup.csh`
+  if ( -e ${shellscr} ) then
+    source ${shellscr} ${*:q}
+  endif
+  unset shellscr
+
+  # login part. has to deal with the environment. The script can be called manually from 
+  # .login
+  if ($?loginsh) then
+    set loginscr=`/usr/bin/which Euclid_group_login.csh`
+    if ( -e ${loginscr} ) then
+      source ${loginscr} ${*:q}
+    endif
+    unset loginscr
+  endif
+
 endif
 
 
--- data/profile/euclid.sh
+++ data/profile/euclid.sh
@@ -4,6 +4,23 @@ if [[ ! -e ${HOME}/.noEuclidLoginScript ]]; then
     export EUCLID_SYS_CONFIG=/etc/sysconfig/euclid
   fi
 
+  # shell part. has to deal with the shell settings. Pretty much everything but 
+  # the environment variables. The script can be manually called from .bashrc
+  shellscr=`/usr/bin/which Euclid_group_setup.sh`
+  if  [[ -e ${shellscr} ]]; then
+     . ${shellscr} "$@"
+  fi
+  unset shellscr
+
+  # login part. has to deal with the environment. The script can be called manually from 
+  # .bash_profile
+  if (shopt -q login_shell || [[ -o login ]]) 2> /dev/null ; then
+    loginscr=`/usr/bin/which Euclid_group_login.sh`
+    if [[ -e ${loginscr} ]]; then
+       . ${loginscr} "$@"
+    fi
+    unset loginscr
+  fi
 
 fi
 
