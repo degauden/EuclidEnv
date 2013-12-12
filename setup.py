41d5173aeed1aa98df09faa7f701c1dfe0776473
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2013-12-12 13:37:46 +0100

    Fixed typo for the top location

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup (name="euclidenv",
        data_files = [("/etc/profile.d", [os.path.join("data", "profile", "euclid.sh"),
                                          os.path.join("data", "profile", "euclid.csh")]),
                      ("/etc/sysconfig", [os.path.join("data", "sys", "config", "euclid")]),
-                     ("share/EuclidEnv/cmake", [os.path.join("data", "cmake", "ElementsProjectConfig.cmake")])
+                     ("EuclidEnv/cmake", [os.path.join("data", "cmake", "ElementsProjectConfig.cmake")])
                      ],
 
 #       options = {'bdist_rpm':{'post_install' : 'post_install',
