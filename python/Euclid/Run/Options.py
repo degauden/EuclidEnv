cd4b453ef3b94bfec3491c3d3c7ea8ba02d86099
hdegaude <hdegaude@463500fe-d6be-4184-9b20-47717e75ecac>
2014-04-07 15:12:40 +0000

    remove useless code
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/EuclidEnv/trunk@4437 463500fe-d6be-4184-9b20-47717e75ecac

--- python/Euclid/Run/Options.py
+++ python/Euclid/Run/Options.py
@@ -10,40 +10,20 @@ class SearchPathEntry(object):
     def __str__(self):
         return str(self.path)
 
-class LHCbDevPathEntry(object):
-    def __str__(self):
-        return str(os.environ['LHCBDEV'])
-
-
 def addSearchPath(parser):
     '''
     Common options used to extend the search path.
     '''
-    from optparse import OptionValueError
 
     def dev_dir_cb(option, opt_str, value, parser):
-        if value is None:
-            if 'LHCBDEV' in os.environ:
-                value = LHCbDevPathEntry()
-            else:
-                raise OptionValueError('--dev used, but LHCBDEV is not defined')
-        else:
-            value = SearchPathEntry(value)
+        value = SearchPathEntry(value)
         parser.values.dev_dirs.append(value)
 
-    parser.add_option('--dev', action='callback',
-                      callback=dev_dir_cb,
-                      help='prepend $LHCBDEV to the search path. '
-                           'Note: the directories are searched in the '
-                           'order specified on the command line.')
     parser.add_option('--dev-dir', action='callback', metavar='DEVDIR',
                       type='string', callback=dev_dir_cb,
                       help='prepend DEVDIR to the search path. '
                            'Note: the directories are searched in the '
                            'order specified on the command line.')
-
-
-
 
     parser.add_option('--user-area', action='store',
                       help='Use the specified path as User_release_area instead of '
@@ -53,7 +33,7 @@ def addSearchPath(parser):
                       help='Ignore the user release area when looking for projects.')
 
     parser.set_defaults(dev_dirs=[],
-                        user_area=os.environ.get('User_release_area'),
+                        user_area=os.environ.get('User_area'),
                         no_user_area=False)
 
     return parser
@@ -82,14 +62,12 @@ def addPlatform(parser):
     Add option to specify a platform.
     '''
 
-    parser.add_option('-c', '--platform',
+    parser.add_option('-b', '--platform',
                       help='runtime platform [default: %default]')
 
 
     if 'BINARY_TAG' in os.environ:
         platform = os.environ['BINARY_TAG']
-    elif 'CMTCONFIG' in os.environ:
-        platform = os.environ['CMTCONFIG']
     else:
         # auto-detect
         from Euclid.Platform import NativeMachine
