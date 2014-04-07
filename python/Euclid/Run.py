ec1e4bd7eb6148a6049efc3fa060b3b3c8a60780
hdegaude <hdegaude@463500fe-d6be-4184-9b20-47717e75ecac>
2014-04-07 15:12:06 +0000

    add the core of the Euclid Run script modules
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/EuclidEnv/trunk@4428 463500fe-d6be-4184-9b20-47717e75ecac

--- python/Euclid/Run.py
+++
@@ -1,8 +0,0 @@
-'''
-Created on Mar 19, 2014
-
-@author: hubert
-'''
-
-def execute(project, command, version="latest"):
-    pass
---
+++ python/Euclid/Run/lookup.py
@@ -0,0 +1,119 @@
+###############################################################################
+# (c) Copyright 2013 CERN                                                     #
+#                                                                             #
+# This software is distributed under the terms of the GNU General Public      #
+# Licence version 3 (GPL Version 3), copied verbatim in the file "COPYING".   #
+#                                                                             #
+# In applying this licence, CERN does not waive the privileges and immunities #
+# granted to it by virtue of its status as an Intergovernmental Organization  #
+# or submit itself to any jurisdiction.                                       #
+###############################################################################
+__author__ = 'Marco Clemencic <marco.clemencic@cern.ch>'
+
+import os
+import logging
+
+# FIXME: when we drop Python 2.4, this should become 'from . import path'
+from Euclid.Run import path, Error
+
+log = logging.getLogger(__name__)
+
+class NotFoundError(Error):
+    '''
+    Generic error for configuration elements that are not found.
+    '''
+
+class MissingManifestError(NotFoundError):
+    '''
+    The manifest.xml for a project was not found.
+    '''
+
+class MissingProjectError(NotFoundError):
+    '''
+    The manifest.xml for a project was not found.
+    '''
+    def __init__(self, *args):
+        super(MissingProjectError, self).__init__(*args)
+        self.name, self.version, self.platform, self.path = args
+    def __str__(self):
+        return 'cannot find project {0} {1} for {2} in {3}'.format(*self.args)
+
+def findProject(name, version, platform):
+    '''
+    Find a Gaudi-based project in the directories specified in the 'path'
+    variable.
+
+    @param name: name of the project (case sensitive for local projects)
+    @param version: version of the project
+    @param platform: binary platform id
+
+    @return path to the project binary directory
+    '''
+    log.debug('findProject(%r, %r, %r)', name, version, platform)
+    # standard project suffixes
+    suffixes = ['{0}_{1}'.format(name, version),
+                os.path.join(name.upper(), '{0}_{1}'.format(name.upper(), version))]
+    # special case: with the default 'latest' version we allow the plain name
+    if version == 'latest':
+        suffixes.insert(0, name)
+
+    bindir = os.path.join('InstallArea', platform)
+    for d in [os.path.join(b, s, bindir)
+              for b in path
+              for s in suffixes]:
+        log.debug('check %s', d)
+        if os.path.exists(d):
+            log.debug('OK')
+            return d
+    else:
+        raise MissingProjectError(name, version, platform, path)
+
+def parseManifest(manifest):
+    '''
+    Extract the list of required projects from a manifest.xml
+    file.
+
+    @param manifest: path to the manifest file
+    @return: tuple with ([projects...],) as (name, version) pairs
+    '''
+    from xml.dom.minidom import parse
+    m = parse(manifest)
+    def _iter(parent, child):
+        '''
+        Iterate over the tags in <parent><child/><child/></parent>.
+        '''
+        for pl in m.getElementsByTagName(parent):
+            for c in pl.getElementsByTagName(child):
+                yield c
+    # extract the list of used (project, version) from the manifest
+    used_projects = [(p.attributes['name'].value, p.attributes['version'].value)
+                     for p in _iter('used_projects', 'project')]
+    return (used_projects,)
+
+def getEnvXmlPath(project, version, platform):
+    '''
+    Return the list of directories to be added to the Env XML search path for
+    a given project.
+    '''
+    pdir = findProject(project, version, platform)
+    search_path = [pdir]
+    # manifests to parse
+    manifests = [os.path.join(pdir, 'manifest.xml')]
+    while manifests:
+        manifest = manifests.pop(0)
+        if not os.path.exists(manifest):
+            raise MissingManifestError(manifest)
+        projects,_ = parseManifest(manifest)
+        # add the project directories ...
+        pdirs = [findProject(p, v, platform) for p, v in projects]
+        search_path.extend(pdirs)
+        # ... and their manifests to the list of manifests to parse
+        manifests.extend([os.path.join(pdir, 'manifest.xml') for pdir in pdirs])
+    def _unique(iterable):
+        returned = set()
+        for i in iterable:
+            if i not in returned:
+                returned.add(i)
+                yield i
+    return list(_unique(search_path))
+
