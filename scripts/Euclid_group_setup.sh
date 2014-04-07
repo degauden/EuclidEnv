437f4e1d9496039468fde23878ac88c76b6d357b
hdegaude <hdegaude@463500fe-d6be-4184-9b20-47717e75ecac>
2014-04-07 15:11:34 +0000

    fix the use of local variable with the same name
    This is especially critical for (t)csh but I do it also for the bash
    familiy as well.
    
    git-svn-id: http://euclid.esac.esa.int/svn/EC/SGS/SDC/CH/Projects/EuclidEnv/trunk@4418 463500fe-d6be-4184-9b20-47717e75ecac

--- scripts/Euclid_group_setup.sh
+++ scripts/Euclid_group_setup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 if [[ ! -e ${HOME}/.noEuclidLoginScript ]]; then
 
-  my_own_prefix="%(this_install_prefix)s"
+  my_own_prefix3="%(this_install_prefix)s"
 
   if [[ ! -n "$EUCLID_CONFIG_FILE" ]]; then
-    if [[ -r ${my_own_prefix}/bin/Euclid_config.sh ]]; then
-      confscr=${my_own_prefix}/bin/Euclid_config.sh
+    if [[ -r ${my_own_prefix3}/bin/Euclid_config.sh ]]; then
+      confscr=${my_own_prefix3}/bin/Euclid_config.sh
     else
       confscr=`/usr/bin/which Euclid_config.sh`
     fi
@@ -13,14 +13,14 @@ if [[ ! -e ${HOME}/.noEuclidLoginScript ]]; then
     unset confscr
   fi
 
-  if [[ -r ${my_own_prefix}/bin/ELogin.sh ]]; then
-    elogscr=${my_own_prefix}/bin/ELogin.sh
+  if [[ -r ${my_own_prefix3}/bin/ELogin.sh ]]; then
+    elogscr=${my_own_prefix3}/bin/ELogin.sh
   else
     elogscr=`/usr/bin/which ELogin.sh`
   fi    
   if [[ -e ${elogscr} ]]; then
   
-    needs_cleanup=no
+    needs_cleanup3=no
   
     if [[ -n "$ELOGIN_DONE" ]]; then
       # The login part has already been done. Only the shell (setup) part is redone.
@@ -33,15 +33,15 @@ if [[ ! -e ${HOME}/.noEuclidLoginScript ]]; then
       
       if [[ ! -n "$EUCLID_POST_DONE" ]]; then
         if [[ -n "$EUCLID_POST_SCRIPT" ]]; then
-          if [[ -r ${my_own_prefix}/bin/${EUCLID_POST_SCRIPT}.sh ]]; then
-            epostscr=${my_own_prefix}/bin/${EUCLID_POST_SCRIPT}.sh
+          if [[ -r ${my_own_prefix3}/bin/${EUCLID_POST_SCRIPT}.sh ]]; then
+            epostscr=${my_own_prefix3}/bin/${EUCLID_POST_SCRIPT}.sh
           else
             epostscr=`/usr/bin/which ${EUCLID_POST_SCRIPT}.sh`
           fi
           if [[ -r ${epostscr} ]]; then
             . ${epostscr} "$@" >> ${E_BANNER}
             export EUCLID_POST_DONE=yes
-            needs_cleanup=yes
+            needs_cleanup3=yes
           fi
           unset epostscr
         fi
@@ -49,10 +49,10 @@ if [[ ! -e ${HOME}/.noEuclidLoginScript ]]; then
                 
     fi
     
-    if [[ "$needs_cleanup" = "yes" ]]; then
+    if [[ "$needs_cleanup3" = "yes" ]]; then
       if [[ "x$E_NO_STRIP_PATH" ==  "x" ]] ; then
-        if [[ -r ${my_own_prefix}/bin/StripPath.sh ]]; then
-          stripscr=${my_own_prefix}/bin/StripPath.sh
+        if [[ -r ${my_own_prefix3}/bin/StripPath.sh ]]; then
+          stripscr=${my_own_prefix3}/bin/StripPath.sh
         else
           stripscr=`/usr/bin/which StripPath.sh`
         fi
@@ -61,12 +61,12 @@ if [[ ! -e ${HOME}/.noEuclidLoginScript ]]; then
       fi
     fi    
     
-    unset needs_cleanup
+    unset needs_cleanup3
     
   fi
   unset elogscr
     
-  unset my_own_prefix
+  unset my_own_prefix3
 
 fi
 
