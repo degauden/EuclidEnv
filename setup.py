94ff6bbf44c6fc2c61aab3b3c29a55609b907644
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2014-06-10 16:01:39 +0200

    release of version 1.3

--- setup.py
+++ setup.py
@@ -101,7 +101,7 @@ class my_install(_install):
                 
 
 setup (name="EuclidEnv",
-       version="1.2.2",
+       version="1.3",
        description="Euclid Environment Scripts",
        author="Hubert Degaudenzi",
        author_email="Hubert.Degaudenzi@unige.ch",
