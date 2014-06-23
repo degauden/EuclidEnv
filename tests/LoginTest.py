37e309f4dcb34c8688c47f4f82ae259f035be62a
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2014-06-23 11:57:59 +0200

    Add empty test file for the Login features

---
+++ tests/LoginTest.py
@@ -0,0 +1,18 @@
+from Euclid.Login import getLoginEnv, getLoginAliases, getLoginExtra
+
+
+import unittest
+
+class LoginTestCase(unittest.TestCase):
+
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+
+    def tearDown(self):
+        unittest.TestCase.tearDown(self)
+
+    def testMet1(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
