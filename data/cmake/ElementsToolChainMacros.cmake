4feb366f7703ad9497d1cec4edb02a900419ba60
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2015-07-24 17:12:54 +0200

    Fix the name of the search path variable

--- data/cmake/ElementsToolChainMacros.cmake
+++ data/cmake/ElementsToolChainMacros.cmake
@@ -241,39 +241,39 @@ function(_internal_find_projects2 projects_var config_file)
         list(REMOVE_AT project_dep_list 0 1)
         string(TOUPPER ${name} name_upper)
         # look for the configuration file of the project
-        string(REPLACE ":" ";" path_list $ENV{PATH})
+        string(REPLACE ":" ";" path_list $ENV{CMAKE_PROJECT_PATH})
         foreach(pth ${path_list})
   
           set(suffixes)
           get_installed_project_suffixes(${name} ${version} ${BINARY_TAG} ${SGS_SYSTEM} suffixes)
-          foreach(suf ${suffixes})
-            set(tmp_cfg_file "${pth}/${suf}/${name}Config.cmake")
-            if(EXISTS ${tmp_cfg_file})
-              set(${name_upper}_CONFIG_FILE ${tmp_cfg_file})
-              break()
-            endif()
-          endforeach()
-#          find_file(${name_upper}_CONFIG_FILE NAMES ${name}Config.cmake
-#                    PATH_SUFFIXES ${suffixes}
-#                    PATHS ${pth}
-#                    NO_DEFAULT_PATH)
+#          foreach(suf ${suffixes})
+#            set(tmp_cfg_file "${pth}/${suf}/${name}Config.cmake")
+#            if(EXISTS ${tmp_cfg_file})
+#              set(${name_upper}_CONFIG_FILE ${tmp_cfg_file})
+#              break()
+#            endif()
+#          endforeach()
+          find_file(${name_upper}_CONFIG_FILE NAMES ${name}Config.cmake
+                    PATH_SUFFIXES ${suffixes}
+                    PATHS ${pth}
+                    NO_DEFAULT_PATH)
                   
               
           if(NOT ${name_upper}_CONFIG_FILE)
             # lookup a project without a version subdir
             set(suffixes)
             get_installed_versionless_project_suffixes(${name} ${BINARY_TAG} ${SGS_SYSTEM} suffixes)
-            foreach(suf ${suffixes})
-              set(tmp_cfg_file "${pth}/${suf}/${name}Config.cmake")
-              if(EXISTS ${tmp_cfg_file})
-                set(${name_upper}_CONFIG_FILE ${tmp_cfg_file})
-                break()
-              endif()
-            endforeach()
-#            find_file(${name_upper}_CONFIG_FILE NAMES ${name}Config.cmake
-#                      PATH_SUFFIXES ${suffixes}
-#                      PATHS ${pth}
-#                      NO_DEFAULT_PATH)
+#            foreach(suf ${suffixes})
+#              set(tmp_cfg_file "${pth}/${suf}/${name}Config.cmake")
+#              if(EXISTS ${tmp_cfg_file})
+#                set(${name_upper}_CONFIG_FILE ${tmp_cfg_file})
+#                break()
+#              endif()
+#            endforeach()
+            find_file(${name_upper}_CONFIG_FILE NAMES ${name}Config.cmake
+                      PATH_SUFFIXES ${suffixes}
+                      PATHS ${pth}
+                      NO_DEFAULT_PATH)
             # check the internal version
             set(match_found FALSE)
             if(${${name_upper}_CONFIG_FILE})           
