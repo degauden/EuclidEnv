5dfe6d90d80819c2cf6e5ac8b9a2e85aa830decf
Hubert Degaudenzi <Hubert.Degaudenzi@unige.ch>
2018-11-27 10:03:28 +0100

    Add fake implementation of the Euclid Kernel
    It is a copy of the echo kernel for the moment.

--- python/Euclid/Kernel.py
+++ python/Euclid/Kernel.py
@@ -21,3 +21,33 @@ along with this library; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
 '''
+from ipykernel.kernelbase import Kernel
+
+class EuclidKernel(Kernel):
+    implementation = 'Euclid'
+    implementation_version = '1.0'
+    language = 'no-op'
+    language_version = '0.1'
+    language_info = {
+        'name': 'Any text',
+        'mimetype': 'text/plain',
+        'file_extension': '.txt',
+    }
+    banner = "Euclid kernel - as useful as a parrot"
+
+    def do_execute(self, code, silent, store_history=True, user_expressions=None,
+                   allow_stdin=False):
+        if not silent:
+            stream_content = {'name': 'stdout', 'text': code}
+            self.send_response(self.iopub_socket, 'stream', stream_content)
+
+        return {'status': 'ok',
+                # The base class increments the execution count
+                'execution_count': self.execution_count,
+                'payload': [],
+                'user_expressions': {},
+               }
+
+if __name__ == '__main__':
+    from ipykernel.kernelapp import IPKernelApp
+    IPKernelApp.launch_instance(kernel_class=EuclidKernel)
