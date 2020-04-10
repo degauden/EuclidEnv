e45eafb1292c84d95165de52fffa5d3243f14ef4
Hubert Degaudenzi <Hubert.Degaudenzi@gmail.com>
2020-04-10 11:41:29 +0200

    Review the cleanup method

--- python/Euclid/__init__.py
+++ python/Euclid/__init__.py
@@ -2,6 +2,7 @@
 This is the initial setup for the Euclid namespace package
 """
 from pkgutil import extend_path
+
 __path__ = extend_path(__path__, __name__)  # @ReservedAssignment
 
 __author__  = 'Hubert Degaudenzi'
@@ -10,35 +11,38 @@ import sys
 
 # hooks and other customizations are not used ith iPython
 _is_ipython = hasattr(__builtins__, '__IPYTHON__') or 'IPython' in sys.modules
+_cleanup_list = []
 
 # Add some infrastructure if we are being imported via a Jupyter Kernel ------
 if _is_ipython:
     from IPython import get_ipython
-    ip = get_ipython()
-    if hasattr(ip,"kernel"):
+    _ip = get_ipython()
+    _cleanup_list.append(_ip)
+    if hasattr(_ip,"kernel"):
         from .Login import LoginScript
-        # option_list = ["--debug"]
-        option_list = []
+        # _option_list = ["--debug"]
+        _option_list = []
+        _cleanup_list.append(_option_list)
         _login_script = LoginScript()
-        _login_script.parseOpts(option_list)
+        _cleanup_list.append(_login_script)
+        _login_script.parseOpts(_option_list)
         _ev = _login_script.setEnv()
+        _cleanup_list.append(_ev)
         _al = _login_script.setAliases()
+        _cleanup_list.append(_al)
         _ex = _login_script.setExtra()
+        _cleanup_list.append(_ex)
 
-# cleanup 
+# _cleanup 
 import atexit
-def cleanup():
+def _cleanup():
 
-    if _is_ipython:
-        ip = get_ipython()
-        if hasattr(ip,"kernel"):
-            del _ex
-            del _al
-            del _ev
-            del _login_script
+    for s in _cleanup_list:
+        del s
 
-    # destroy ROOT module
+    del _cleanup_list, _is_ipython
+    # destroy Euclid module
     del sys.modules[ 'Euclid' ]
 
-atexit.register( cleanup )
-del cleanup, atexit
+atexit.register( _cleanup )
+del _cleanup, atexit, sys
